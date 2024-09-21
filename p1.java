40a2ca1fa2f72b6b0d484ff64cc29f8ea92aff47
Prathamesh <prathmeshdiwane29@gmail.com>
2024-09-21 23:41:38 +0530

    p1.java

---
+++ p1.java
@@ -0,0 +1,8 @@
+class Deno{
+	public static void main(String[]args){
+
+		System.out.println("Hello Prathamesh");
+	}
+
+
+}
---
+++ p2.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String[]args){
+		int a = 3;
+		int b = 5;
+		System.out.println(a=b);
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
